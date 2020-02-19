1ea9f5613114682b934e82ea7091ece76364eb6a
namihaha77 <47521419+namihaha77@users.noreply.github.com>
2020-02-19 22:12:08 +0400

    solve the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let minNum = array[0];
+  array.forEach(element => {
+    if (element < minNum) {
+      minNum = element;
+    }
+  });
+  return minNum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let maxNum = array[0];
+  array.forEach(element => {
+    if (element > maxNum) {
+      maxNum = element;
+    }
+  });
+  return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  array.forEach(element => {
+    sum += element;
+  });
+  return sum / array.length;
 }
